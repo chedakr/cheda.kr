a7b53f735973b3187329677b0ddb6fc3cd504687
Xvezda <Xvezda@users.noreply.github.com>
2024-04-16 10:16:22 +0000

    deploy: 011a7ea5a85c944cd91cf8bfc84d440948e76236

---
+++ _next/static/7tAUodEPsQkJGwI_bSRDw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e8bcbe52b5df2a23.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/chunks/pages/_error-6650f905c333de20.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{3416:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(6189)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=3416)}),_N_E=n.O()}]);
